56da200bd45ed627ae0fce228f0b4b18424568dd
Nicolás López <nicolaslopezj@me.com>
2015-07-06 04:01:08 -0300

    dates likes-history

--- client/views/clients/reports/show/data/fb/likes-history.js
+++ client/views/clients/reports/show/data/fb/likes-history.js
@@ -1,21 +1,16 @@
 Template['reportsData_fb:likes-history'].onRendered(function() {
-  var data = {
-    labels: [],
-    datasets: [{
-      label: 'My FB Page',
-      data: []
-    }]
-  };
-
-  _.each(this.data.reverse(), function(point){
-    var date = moment(point.date).format("D-MMM");
-    data.labels.push(date);
-    data.datasets[0].data.push(point.likes);
+  var self = this;
+  _.each(this.data, function(chart){
+    var ctx = self.$('#likes-chart-' + chart.targetId).get(0).getContext("2d");
+    var chart = new Chart(ctx).Line(chart, {
+      datasetFill: false
+    });
   })
-
-  var ctx = this.$("canvas").get(0).getContext("2d");
-  var chart = new Chart(ctx).Line(data, {
-    scaleShowGridLines: false,
-    datasetFill : false,
-  });
 })
+
+
+Template['reportsData_fb:likes-history'].helpers({
+  charts: function() {
+    return this;
+  }
+});
